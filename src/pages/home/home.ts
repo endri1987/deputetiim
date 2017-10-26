cb1de69b5ce7828933bf2421cd2b4a6b56226a94
klidisp2512 <klidi.sp2512@gmail.com>
2017-10-26 16:27:27 +0200

    iconat ok

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -21,22 +21,22 @@ import { SondazhedhevlerësimePage } from '../sondazhedhevlerësime/sondazhedhev
 export class HomePage {
 
   pages: Array<{title: string, component: any, icon: string, bg: string }>;
-  graphics: string = "../../graphics/";
+  graphics: string = "assets/img/";
 
   constructor(public navCtrl: NavController) {
     this.pages = [
-      { title: 'Parlamenti im', component: ParlamentiPage, icon: 'bicycle', bg: 'parlamenti animated fadeInRight'},
-      { title: 'Deputeti im', component: DeputetiPage, icon: 'home' , bg: 'deputeti'},
-      { title: 'Studime dhe Raporte', component: RaportePage, icon: 'bowtie', bg: 'studime' },
-      { title: 'Pyet Deputetin', component: PyetPage, icon: 'build' , bg: 'pyetDeputetin'},
-      { title: 'Gjithcka Mbi Ligjet', component: LigjetPage, icon: 'cafe', bg: 'gjithckaLigjet' },
-      { title: 'Seancat Plenare', component: SeancatPage, icon: 'flag', bg: 'seancat' },
-      { title: 'Komisionet Parlamentare', component: KomisionetPage, icon: 'flag', bg: 'komisionetParlamentare' },
-      { title: 'Transparenca financiare', component: TransparencaPage, icon: 'bicycle', bg: 'transparenca' },
-      { title: 'Gratë Deputete', component: GrateDeputetePage, icon: 'woman', bg: 'grateDeputete' },
-      { title: 'Grupet Parlamentare', component: GrupetParlamentarePage, icon: 'bowtie', bg: 'grupetParlamentare' },
-      { title: 'Institucionet e Pavaruara', component: InstitucionetPavaruaraPage, icon: 'bowtie', bg: 'institucionet' },
-      { title: 'Sondazhe dhe vlerësime', component: SondazhedhevlerësimePage, icon: 'md-contacts' , bg: 'sondazhe'},
+      { title: 'Parlamenti im', component: ParlamentiPage, icon: 'parlamentiim.png', bg: 'parlamenti animated fadeInRight'},
+      { title: 'Deputeti im', component: DeputetiPage, icon: 'deputetiim.png' , bg: 'deputeti'},
+      { title: 'Studime dhe Raporte', component: RaportePage, icon: 'studimeRaporte.png', bg: 'studime' },
+      { title: 'Pyet Deputetin', component: PyetPage, icon: 'pyetDeputetin.png' , bg: 'pyetDeputetin'},
+      { title: 'Gjithcka Mbi Ligjet', component: LigjetPage, icon: 'gjithckaLigjet.png', bg: 'gjithckaLigjet' },
+      { title: 'Seancat Plenare', component: SeancatPage, icon: 'seancatPlenare.png', bg: 'seancat' },
+      { title: 'Komisionet Parlamentare', component: KomisionetPage, icon: 'komisionetParlamentare.png', bg: 'komisionetParlamentare' },
+      { title: 'Transparenca financiare', component: TransparencaPage, icon: 'transparenca.png', bg: 'transparenca' },
+      { title: 'Gratë Deputete', component: GrateDeputetePage, icon: 'grateDeputete.png', bg: 'grateDeputete' },
+      { title: 'Grupet Parlamentare', component: GrupetParlamentarePage, icon: 'grupetParlamentare.png', bg: 'grupetParlamentare' },
+      { title: 'Institucionet e Pavaruara', component: InstitucionetPavaruaraPage, icon: 'institucionetPavarura.png', bg: 'institucionet' },
+      { title: 'Sondazhe dhe vlerësime', component: SondazhedhevlerësimePage, icon: 'sondazhe.png' , bg: 'sondazhe'},
     ]; 
   }
 ipt
